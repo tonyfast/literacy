643f0ef562019cc6c3c6a28c4a210e6433c7845d
tonyfast <tony.fast@gmail.com>
2017-06-27 23:44:49 -0400

    Remove imports and default repl is a template

--- literacy/__init__.py
+++ literacy/__init__.py
@@ -1,13 +1,13 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 try:
-    from repl import unload_ipython_extension, load_ipython_extension
+    from template import unload_ipython_extension, load_ipython_extension
 except:
-    from .repl import unload_ipython_extension, load_ipython_extension
+    from .template import unload_ipython_extension, load_ipython_extension
 
 
 # In[ ]:
@@ -15,4 +15,10 @@ except:
 
 if __name__ == '__main__':
     get_ipython().system('jupyter nbconvert --to python __init__.ipynb')
+
+
+# In[ ]:
+
+
+
 
--- literacy/preprocessors.py
+++ literacy/preprocessors.py
@@ -1,102 +1,109 @@
-
 # coding: utf-8
 
-# In[1]:
+# In[4]:
 
-
-try: 
+try:
     from markdown import renderer
 except:
     from .markdown import renderer
 
-
-# In[2]:
-
+# In[5]:
 
 from nbformat import NotebookNode
 from nbformat.v4 import new_notebook, new_code_cell, new_markdown_cell
 from nbconvert.filters import comment_lines
 from nbconvert.preprocessors import Preprocessor
+from functools import partial, wraps
 from traitlets import Any, Unicode
+from inspect import getfullargspec
 __all__ = ['Explode', 'Dedent']
-
-
-# In[3]:
-
-
-class NumberCell(Preprocessor):
-    key = Unicode(default_value="_")
-    def preprocess_cell(self, cell, resources, index):
-        return cell['metadata'].update({self.key:index}) or cell, resources
-
-
-# In[4]:
-
-
-class Explode(NumberCell):
-    def preprocess(self, nb, resources):
-        cells = []
-        for cell in super().preprocess(nb, resources)[0]['cells']:
-            if cell['cell_type']=='code':
-                new = renderer.render(cell, cell['metadata'])
-                cells.extend(new['cells'])
-            else:
-                cells.append(cell)
-        return nb.update(cells=cells) or nb, resources
-
-
-# In[5]:
-
-
-class SplitSource(Preprocessor):
-    def preprocess_cell(self, cell, resources, index):
-        if isinstance(cell['source'], str):
-            cell['source'] = cell['source'].splitlines()
-        return cell, resources
-
 
 # In[6]:
 
 
-class JoinSource(Preprocessor):
-    def preprocess_cell(self, cell, resources, index):
-        if not isinstance(cell['source'], str):
-            cell['source'] = '\n'.join(cell['source'])
-        return cell, resources
+def preprocessor(function, base=Preprocessor):
+    """A decorator to create nbconvert preprocessors."""
+    args = getfullargspec(function).args
+    if not args[0] == 'self':
+        raise ValueError('Preprocessor must define `self`.')
+    return (type(function.__name__, (base, ), {
+        'preprocess' + {
+            4: '_cell',
+            3: ''
+        }[len(args)]: function,
+    }))
 
 
 # In[7]:
+
+
+@preprocessor
+def NumberCell(self, cell, resources, index):
+    return cell['metadata'].update({'index': index}) or cell, resources
+
+
+# In[8]:
+
+
+@preprocessor
+def Explode(self, nb, resources):
+    cells = []
+    for cell in nb['cells']:
+        if cell['cell_type'] == 'code':
+            new = renderer.render(cell, cell['metadata'])
+            cells.extend(new['cells'])
+        else:
+            cells.append(cell)
+    return nb.update(cells=cells) or nb, resources
+
+
+# In[9]:
+
+
+@preprocessor
+def SplitSourceCell(self, cell, resources, index):
+    if isinstance(cell['source'], str):
+        cell['source'] = cell['source'].splitlines()
+    return cell, resources
+
+
+# In[10]:
+
+
+@preprocessor
+def JoinSource(self, cell, resources, index):
+    if not isinstance(cell['source'], str):
+        cell['source'] = '\n'.join(cell['source'])
+    return cell, resources
+
+
+# In[11]:
 
 
 def dedent(lines):
     dedent, out = 0, []
     if isinstance(lines, str):
         lines = lines.splitlines()
-        
+
     if any(map(str.strip, lines)):
         dedent = next(filter(str.strip, lines))
         dedent = len(dedent) - len(dedent.lstrip())
-    
+
     return '\n'.join(map(lambda x: x[dedent:], lines))
 
 
-# In[8]:
+# In[12]:
 
 
-class Dedent(Preprocessor):
-    def preprocess_cell(self, cell, resources, i):
-        return cell.update(source=dedent(cell['source'])) or cell, resources
+@preprocessor
+def Dedent(self, cell, resources, i):
+    return cell.update(source=dedent(cell['source'])) or cell, resources
 
 
 # In[ ]:
-
 
 if __name__ == '__main__':
     get_ipython().system('jupyter nbconvert --to python preprocessors.ipynb')
-
+    get_ipython().system('yapf -i preprocessors.py')
 
 # In[ ]:
-
-
-
-
