04aac0b490a5ecd56325029ef01bfaaa853d3ca7
tonyfast <tony.fast@gmail.com>
2017-06-30 08:51:23 -0400

    Change importable on init

--- literacy/__init__.py
+++ literacy/__init__.py
@@ -1,13 +1,13 @@
 
 # coding: utf-8
 
-# In[15]:
+# In[2]:
 
 
 __import__('importable').load_ipython_extension()
 try:
     from template import unload_ipython_extension, load_ipython_extension
-    import preprocessors
+    import finder
 except:
     from .template import unload_ipython_extension, load_ipython_extension
     from . import preprocessors
@@ -15,9 +15,10 @@ except:
     from . import markdown
     from . import template
     from . import finder
+finder.load_ipython_extension()
 
 
-# In[14]:
+# In[ ]:
 
 
 if __name__ == '__main__':
