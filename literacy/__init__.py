1f0c9a0565821c1f55baf63114ce970ec7c8470f
tonyfast <tony.fast@gmail.com>
2017-06-30 21:03:21 -0400

    Use notebooks for source

--- literacy/__init__.py
+++ literacy/__init__.py
@@ -1,26 +1,2 @@
-
-# coding: utf-8
-
-# In[2]:
-
-
 __import__('importable').load_ipython_extension()
-try:
-    from template import unload_ipython_extension, load_ipython_extension
-    import finder
-except:
-    from .template import unload_ipython_extension, load_ipython_extension
-    from . import preprocessors
-    from . import repl
-    from . import markdown
-    from . import template
-    from . import finder
-finder.load_ipython_extension()
-
-
-# In[ ]:
-
-
-if __name__ == '__main__':
-    get_ipython().system('jupyter nbconvert --to python __init__.ipynb')
-
+from .main import *
