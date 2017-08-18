73b9eac2caa81948e3235848c3292af1eed0ddec
tonyfast <tony.fast@gmail.com>
2017-08-18 08:44:29 -0400

    Default cell is not a template

--- literacy/__init__.py
+++ literacy/__init__.py
@@ -1,7 +1,7 @@
 __import__('importable').load_ipython_extension()
 
 try:
-    from .template import load_ipython_extension
+    from .literate import load_ipython_extension
     from . import literate
     from . import template
     from . import finder
