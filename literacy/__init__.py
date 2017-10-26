30911ecf96039dc7d3d73709aa90326cc26c3f2c
tonyfast <tony.fast@gmail.com>
2017-10-26 11:37:27 -0400

    Create a class to manage the literate programming conversions and add magics
    This code needed to be refactored significantly to work with magics.  The refactor removed code and made the literate programming abstraction clearer.

--- literacy/__init__.py
+++ literacy/__init__.py
@@ -1,10 +1,7 @@
 __import__('importable').load_ipython_extension()
 
-try:
-    from .literate import load_ipython_extension
-    from . import literate
-    from . import template
-except:
-    pass
+from .literate import load_ipython_extension
+from . import literate
+from . import template
 
 Execute = literate.Execute
