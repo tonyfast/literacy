851a9b026a062d9a3d90f28d262b3ca72c99b3b7
tonyfast <tony.fast@gmail.com>
2017-11-06 20:01:48 -0500

    Rename template classes.

--- literacy/template.py
+++ literacy/template.py
@@ -2,27 +2,23 @@
 # coding: utf-8
 
 try:
-    from .literate import Literate, extension
+    from .literate import Markdown, extension
 except:
-    from literate import Literate, extension
+    from literate import Markdown, extension
 from nbconvert.exporters.python import PythonExporter    
 exporter = PythonExporter()
 
 
-class Template(Literate):
+class Jinja2(Markdown):
     def weave(self, code):
         return exporter.environment.from_string(code).render(
             getattr(self.shell, 'user_ns', dict()))
 
 
-load_ipython_extension = extension(Template)
+load_ipython_extension = extension(Jinja2)
 
 
 if __name__ == '__main__':
     load_ipython_extension()
     get_ipython().system('jupyter nbconvert --to python --TemplateExporter.exclude_input_prompt=True template.ipynb')
-
-
-a = 100
-
 
