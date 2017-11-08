d6ef04f5ddd09f280e7cffea57d612b5aaa43cbc
tonyfast <tony.fast@gmail.com>
2017-11-07 22:35:35 -0500

    drop cruft

--- literacy/literate.py
+++ literacy/literate.py
@@ -205,9 +205,3 @@ if __name__ == '__main__':
     load_ipython_extension()
     get_ipython().system('jupyter nbconvert --to python --TemplateExporter.exclude_input_prompt=True literate.ipynb')
 
-
-TESt
-
-    get_ipython().magic('%skip')
-    print(10)
-
